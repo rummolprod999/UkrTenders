11f5d434e16dd60f87cd9b56badc9178084f276d
rummolprod999 <rummolprod999@gmail.com>
2017-11-16 11:06:23 +0300

    update

--- UkrTenders/Settings.fs
+++ UkrTenders/Settings.fs
@@ -1,18 +1,63 @@
 namespace Ukr
-open System.Reflection
+
 open System
 open System.IO
+open System.Reflection
 open System.Xml
 
-module Setting =
-    let GetPathProgram: string = 
-            let path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)
-            if path <> null then
-                path.Substring(5)
-            else ""
-    type T = {Database : string; TempPathTenders: string; LogPathTenders : string; Prefix : string; UserDb : string; PassDb : string; Server : string; Port : int}
+module Setting = 
+    let PathProgram : string = 
+        let path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)
+        if path <> null then path.Substring(5)
+        else ""
     
-    let getSettings ():unit = 
+    type T = 
+        { Database : string
+          TempPathTenders : string
+          LogPathTenders : string
+          Prefix : string
+          UserDb : string
+          PassDb : string
+          Server : string
+          Port : int }
+    
+    let getSettings() : T = 
+        let mutable Database = ""
+        let mutable TempPathTenders = ""
+        let mutable LogPathTenders = ""
+        let mutable Prefix = ""
+        let mutable UserDb = ""
+        let mutable PassDb = ""
+        let mutable Server = ""
+        let mutable Port = 3306
         let xDoc = new XmlDocument()
-        xDoc.Load()
-        ()
+        xDoc.Load(sprintf "%s%csetting_tenders.xml" PathProgram Path.DirectorySeparatorChar)
+        let xRoot = xDoc.DocumentElement
+        if xRoot <> null then 
+            for xnode in xRoot do
+                if (xnode :?> XmlNode).Name = "database" then Database <- (xnode :?> XmlNode).InnerText
+                elif (xnode :?> XmlNode).Name = "tempdir_tenders" then TempPathTenders <- (xnode :?> XmlNode).InnerText
+                elif (xnode :?> XmlNode).Name = "logdir_tenders" then LogPathTenders <- (xnode :?> XmlNode).InnerText
+                elif (xnode :?> XmlNode).Name = "prefix" then Prefix <- (xnode :?> XmlNode).InnerText
+                elif (xnode :?> XmlNode).Name = "userdb" then UserDb <- (xnode :?> XmlNode).InnerText
+                elif (xnode :?> XmlNode).Name = "passdb" then PassDb <- (xnode :?> XmlNode).InnerText
+                elif (xnode :?> XmlNode).Name = "server" then Server <- (xnode :?> XmlNode).InnerText
+                else 
+                    if (xnode :?> XmlNode).Name = "port" then Port <- Int32.Parse((xnode :?> XmlNode).InnerText)
+            { Database = Database
+              TempPathTenders = TempPathTenders
+              LogPathTenders = LogPathTenders
+              Prefix = Prefix
+              UserDb = UserDb
+              PassDb = PassDb
+              Server = Server
+              Port = Port }
+        else 
+            { Database = Database
+              TempPathTenders = TempPathTenders
+              LogPathTenders = LogPathTenders
+              Prefix = Prefix
+              UserDb = UserDb
+              PassDb = PassDb
+              Server = Server
+              Port = Port }

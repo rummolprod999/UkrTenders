3705a6289d5095cbecb6923d48d84237de2f98a2
rummolprod999 <rummolprod999@gmail.com>
2017-11-21 11:44:03 +0300

    update

--- UkrTenders/Parsing.fs
+++ UkrTenders/Parsing.fs
@@ -6,9 +6,10 @@ open Newtonsoft.Json.Linq
 open System
 open System.Collections.Generic
 open System.Data
-open System.Xml
-open System.Text
 open System.Linq
+open System.Text
+open System.Text.RegularExpressions
+open System.Xml
 
 module Parsing = 
     let tenderCount = ref 0
@@ -76,32 +77,132 @@ module Parsing =
                 cmd2.ExecuteNonQuery() |> ignore
                 incr verNum
         ()
-    let TenderKwords (con : MySqlConnection) (idTender : int) (stn : Setting.T) : unit =
+    
+    let TenderKwords (con : MySqlConnection) (idTender : int) (stn : Setting.T) : unit = 
         let resString = new StringBuilder()
-        let selectPurObj = sprintf "SELECT DISTINCT po.name, po.okpd_name FROM %spurchase_object AS po LEFT JOIN %slot AS l ON l.id_lot = po.id_lot WHERE l.id_tender = @id_tender" stn.Prefix stn.Prefix
+        let selectPurObj = 
+            sprintf 
+                "SELECT DISTINCT po.name, po.okpd_name FROM %spurchase_object AS po LEFT JOIN %slot AS l ON l.id_lot = po.id_lot WHERE l.id_tender = @id_tender" 
+                stn.Prefix stn.Prefix
         let cmd1 = new MySqlCommand(selectPurObj, con)
-        cmd1.Prepare();
+        cmd1.Prepare()
         cmd1.Parameters.AddWithValue("@id_tender", idTender) |> ignore
         let dt = new DataTable()
         let adapter = new MySqlDataAdapter()
         adapter.SelectCommand <- cmd1
         adapter.Fill(dt) |> ignore
-        if dt.Rows.Count > 0 then
+        if dt.Rows.Count > 0 then 
             let distrDt = dt.AsEnumerable().Distinct(DataRowComparer.Default)
             for row in distrDt do
                 let name = 
                     match row.IsNull("name") with
                     | true -> ""
-                    | false -> row.["name"]
+                    | false -> string <| row.["name"]
+                
                 let okpdName = 
                     match row.IsNull("okpd_name") with
-                       | true -> ""
-                       | false -> row.["okpd_name"]
-                resString.Add(sprintf "%s %s " name okpdName)
+                    | true -> ""
+                    | false -> string <| row.["okpd_name"]
                 
+                resString.Append(sprintf "%s %s " name okpdName) |> ignore
+        let selectAttach = sprintf "SELECT file_name FROM %sattachment WHERE id_tender = @id_tender" stn.Prefix
+        let cmd2 = new MySqlCommand(selectAttach, con)
+        cmd2.Prepare()
+        cmd2.Parameters.AddWithValue("@id_tender", idTender) |> ignore
+        let dt2 = new DataTable()
+        let adapter2 = new MySqlDataAdapter()
+        adapter2.SelectCommand <- cmd2
+        adapter2.Fill(dt2) |> ignore
+        if dt2.Rows.Count > 0 then 
+            let distrDt = dt2.AsEnumerable().Distinct(DataRowComparer.Default)
+            for row in distrDt do
+                let attName = 
+                    match row.IsNull("file_name") with
+                    | true -> ""
+                    | false -> string <| row.["file_name"]
+                resString.Append(sprintf " %s" attName) |> ignore
+        let idOrg = ref 0
+        let selectPurInf = 
+            sprintf "SELECT purchase_object_info, id_organizer FROM %stender WHERE id_tender = @id_tender" stn.Prefix
+        let cmd3 = new MySqlCommand(selectPurInf, con)
+        cmd3.Prepare()
+        cmd3.Parameters.AddWithValue("@id_tender", idTender) |> ignore
+        let dt3 = new DataTable()
+        let adapter3 = new MySqlDataAdapter()
+        adapter3.SelectCommand <- cmd3
+        adapter3.Fill(dt3) |> ignore
+        if dt3.Rows.Count > 0 then 
+            for row in dt3.Rows do
+                let purOb = 
+                    match row.IsNull("purchase_object_info") with
+                    | true -> ""
+                    | false -> string <| row.["purchase_object_info"]
+                idOrg := match row.IsNull("id_organizer") with
+                         | true -> 0
+                         | false -> row.["id_organizer"] :?> int
+                resString.Append(sprintf " %s" purOb) |> ignore
+        match (!idOrg) <> 0 with
+        | true -> 
+            let selectOrg = 
+                sprintf "SELECT full_name, inn FROM %sorganizer WHERE id_organizer = @id_organizer" stn.Prefix
+            let cmd4 = new MySqlCommand(selectOrg, con)
+            cmd4.Prepare()
+            cmd4.Parameters.AddWithValue("@id_organizer", !idOrg) |> ignore
+            let dt4 = new DataTable()
+            let adapter4 = new MySqlDataAdapter()
+            adapter4.SelectCommand <- cmd4
+            adapter4.Fill(dt4) |> ignore
+            if dt4.Rows.Count > 0 then 
+                for row in dt4.Rows do
+                    let innOrg = 
+                        match row.IsNull("inn") with
+                        | true -> ""
+                        | false -> string <| row.["inn"]
+                    
+                    let nameOrg = 
+                        match row.IsNull("full_name") with
+                        | true -> ""
+                        | false -> string <| row.["full_name"]
+                    
+                    resString.Append(sprintf " %s %s" innOrg nameOrg) |> ignore
+        | false -> ()
+        let selectCustomer = 
+            sprintf 
+                "SELECT DISTINCT cus.inn, cus.full_name FROM %scustomer AS cus LEFT JOIN %spurchase_object AS po ON cus.id_customer = po.id_customer LEFT JOIN %slot AS l ON l.id_lot = po.id_lot WHERE l.id_tender = @id_tender" 
+                stn.Prefix stn.Prefix stn.Prefix
+        let cmd6 = new MySqlCommand(selectCustomer, con)
+        cmd6.Prepare()
+        cmd6.Parameters.AddWithValue("@id_tender", idTender) |> ignore
+        let dt5 = new DataTable()
+        let adapter5 = new MySqlDataAdapter()
+        adapter5.SelectCommand <- cmd6
+        adapter5.Fill(dt5) |> ignore
+        if dt5.Rows.Count > 0 then 
+            let distrDt = dt5.AsEnumerable().Distinct(DataRowComparer.Default)
+            for row in distrDt do
+                let innC = 
+                    match row.IsNull("inn") with
+                    | true -> ""
+                    | false -> string <| row.["inn"]
                 
+                let fullNameC = 
+                    match row.IsNull("full_name") with
+                    | true -> ""
+                    | false -> string <| row.["full_name"]
                 
+                resString.Append(sprintf " %s %s" innC fullNameC) |> ignore
+        let mutable resS = Regex.Replace(resString.ToString(), @"\s+", " ")
+        resS <- (resS.Trim())
+        let updateTender = 
+            sprintf "UPDATE %stender SET tender_kwords = @tender_kwords WHERE id_tender = @id_tender" stn.Prefix
+        let cmd5 = new MySqlCommand(updateTender, con)
+        cmd5.Prepare()
+        cmd5.Parameters.AddWithValue("@id_tender", idTender) |> ignore
+        cmd5.Parameters.AddWithValue("@tender_kwords", resS) |> ignore
+        let res = cmd5.ExecuteNonQuery()
+        if res <> 1 then Logging.Log.logger ("Не удалось обновить tender_kwords", idTender)
         ()
+    
     let ParserT (d : JToken) (stn : Setting.T) (con : MySqlConnection) : unit = 
         con.Open()
         let id = teststring <| d.SelectToken("id")
@@ -237,6 +338,7 @@ module Parsing =
                     cmd7.ExecuteNonQuery() |> ignore
                     idEtp := int cmd7.LastInsertedId
             let startDate = testdate <| JsonConvert.SerializeObject(jsn.SelectToken("data.tenderPeriod.startDate"))
+            //let startDate = enquiryPeriodstartDate
             let endDate = testdate <| JsonConvert.SerializeObject(jsn.SelectToken("data.tenderPeriod.endDate"))
             let biddingDate = testdate <| JsonConvert.SerializeObject(jsn.SelectToken("data.auctionPeriod.startDate"))
             let scoringDate = DateTime.MinValue
@@ -379,16 +481,21 @@ module Parsing =
             with ex -> 
                 Logging.Log.logger "Ошибка добавления версий тендера"
                 Logging.Log.logger ex
-            printfn "%d" !idTender
+            try 
+                TenderKwords con (!idTender) stn
+            with ex -> 
+                Logging.Log.logger "Ошибка добавления kwords тендера"
+                Logging.Log.logger ex
         ()
     
+    //printfn "%d" !idTender
     let parserL (o : JObject) (stn : Setting.T) (connectstring : string) : unit = 
-        let uri = teststring <| o.SelectToken("next_page.uri")
+        //let uri = teststring <| o.SelectToken("next_page.uri")
         //printfn "%A" uri
         let tdata = o.SelectToken("data") :?> JArray
         for d in tdata do
+            let id = d.SelectToken("id")
             try 
-                let id = d.SelectToken("id")
                 using (new MySqlConnection(connectstring)) (ParserT d stn)
             with ex -> Logging.Log.logger (ex, id)
     
@@ -424,6 +531,6 @@ module Parsing =
                 | (x, cnt) when cnt < 100 -> 
                     ParsungListTenders json st
                     continueLooping <- false
-                | (x, cnt) -> 
+                | (x, _) -> 
                     ParsungListTenders json st
                     urlDown <- x
